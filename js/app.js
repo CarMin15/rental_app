920c9cda7caf94dbc154cfa5807d4fdb9838d380
Maria Rosa <maria.rosa.carrion@gmail.com>
2015-11-09 10:38:38 +0800

    like button

---
+++ js/app.js
@@ -0,0 +1,16 @@
+	/***** HEADER ******/
+	/**** SEARCH BOXES - CALENDAR & BUTTON ******/
+	/***** SEARCH RESULTS - APARTMENTS ******/
+	
+
+
+	/***** HEADER ******/
+
+
+	/**** SEARCH BOXES - CALENDAR & BUTTON ******/
+
+	/***** SEARCH RESULTS - APARTMENTS ******/
+
+$(".like_button").click(function() {
+	$( this ).attr( "src", "images/liked.png" );
+});
