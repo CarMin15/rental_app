f94d8900f65db4ae890924b4e6fc6a5df0c4caa8
Maria Rosa <maria.rosa.carrion@gmail.com>
2015-11-09 16:42:07 +0800

    info cities

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 	/***** HEADER ******/
 	/**** SEARCH BOXES - CALENDAR & BUTTON ******/
 	/***** SEARCH RESULTS - APARTMENTS ******/
-	
 
-
-	/***** HEADER ******/
-
-
-	/**** SEARCH BOXES - CALENDAR & BUTTON ******/
-
-	/***** SEARCH RESULTS - APARTMENTS ******/
 
 $(function() {
+	var infoCities = "http://rental-app-rails.herokuapp.com/cities.json";
+	$.getJSON( infoCities, {})
+	.done(function(data) {
+		console.log(data);
+	})
+
+
 	$(document).on('click', '.like_button', function() {
 		$(this).toggleClass("not_liked");
 	});
+
+	
 });
