6f16b5ec81bf18d6db236f99861148ea5080b3d8
María Rosa Carrión <mariarosa@Marias-MacBook.local>
2015-12-22 17:01:30 +0700

    added cities ajax call

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
-	/***** HEADER ******/
+$(function() {
+
+  /***** HEADER ******/
+
+	$.getJSON("http://rental-app-rails.herokuapp.com/cities.json", function(data) {
+		var html = "";
+
+		$(data).each(function(i, city) {
+			html += "<option value='" + city.slug + "'>" + city.name + "</option>\n";
+		});
+
+		$( "#citiesList" ).html(html);
+	});
+
 	/**** SEARCH BOXES - CALENDAR & BUTTON ******/
 	/***** SEARCH RESULTS - APARTMENTS ******/
-
-
-$(function() {
-	var infoCities = "http://rental-app-rails.herokuapp.com/cities.json";
-	$.getJSON( infoCities, {})
-	.done(function(data) {
-		console.log(data);
-	})
-
 
 	$(document).on('click', '.like_button', function() {
 		$(this).toggleClass("not_liked");
 	});
-
-	
 });
