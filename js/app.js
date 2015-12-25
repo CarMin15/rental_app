0a3c56bf5006a1c7e3199f1b65f105ee8abc478a
María Rosa <CarMin15@users.noreply.github.com>
2015-12-25 18:41:06 +0700

    calendar

--- js/app.js
+++ js/app.js
@@ -15,43 +15,54 @@ $(function() {
 	/**** SEARCH BOXES - CALENDAR & BUTTON ******/
 
 
+// $.datepicker.setDefaults({
+//   showOn: "both",
+//   buttonImageOnly: true,
+//   buttonImage: "calendar.gif",
+//   buttonText: "Calendar"
+// });
 
-$.getJSON("http://rental-app-rails.herokuapp.com/cities/new-york/rentals.json?start_date&end_date5&nb_guests=1", function(data){
-  var html = "";
+  //$.datepick({minDate: new Date(2014, 12-1, 25)});
+  $( "#start_date, #end_date" ).datepicker();
 
-  $(data).each(function(i, apartment) {
-    
-    var liked = (apartment.user_liked) ? "" : "not_liked";  
 
-    var instant_book = (apartment.instant_book) ? "active" : "";
 
-    html += ' \
-    <div class="apartment"> \
-      <div class="apartment_gallery"> \
-        <ul class="gallery"> \
-          <li> \
-            <img src="'+ apartment.pictures[0].url +'" alt="'+ apartment.name +'" class="apartment_p"> \
-          </li> \
-        </ul> \
-        <i class="like_button '+ liked +'"></i> \
-        <div class="price_wrapper"> \
-          <span class="currency">$</span> \
-          <span class="price">' + apartment.price + '</span> \
-          <img src="images/instant_book.png" alt="Instant booking available!" class="instant_book ' + instant_book + '" /> \
+  $.getJSON("http://rental-app-rails.herokuapp.com/cities/new-york/rentals.json?start_date&end_date5&nb_guests=1", function(data){
+    var html = "";
+
+    $(data).each(function(i, apartment) {
+      
+      var liked = (apartment.user_liked) ? "" : "not_liked";  
+
+      var instant_book = (apartment.instant_book) ? "active" : "";
+
+      html += ' \
+      <div class="apartment"> \
+        <div class="apartment_gallery"> \
+          <ul class="gallery"> \
+            <li> \
+              <img src="'+ apartment.pictures[0].url +'" alt="'+ apartment.name +'" class="apartment_p"> \
+            </li> \
+          </ul> \
+          <i class="like_button '+ liked +'"></i> \
+          <div class="price_wrapper"> \
+            <span class="currency">$</span> \
+            <span class="price">' + apartment.price + '</span> \
+            <img src="images/instant_book.png" alt="Instant booking available!" class="instant_book ' + instant_book + '" /> \
+          </div> \
         </div> \
-      </div> \
-      <div class="apartment_description"> \
-        <a href="' + apartment.user.profile_url + '"> \
-          <img src="'+ apartment.user.profile_picture_url + '" alt="' + apartment.user.name + '" class="owner"> \
-        </a> \
-        <a href="#"> \
-          <span class="apartment_name">' + apartment.name + '</span> \
-          <span class="apartment_kind">' + apartment.kind + '</span> \
-          <span class="apartment_score">&bull; ' + apartment.score + '/5</span> \
-          <span class="apartment_reviews">&bull; '+ apartment.reviews_count + ' reviews</span> \
-        </a> \
-      </div> \
-    </div>'; 
+        <div class="apartment_description"> \
+          <a href="' + apartment.user.profile_url + '"> \
+            <img src="'+ apartment.user.profile_picture_url + '" alt="' + apartment.user.name + '" class="owner"> \
+          </a> \
+          <a href="#"> \
+            <span class="apartment_name">' + apartment.name + '</span> \
+            <span class="apartment_kind">' + apartment.kind + '</span> \
+            <span class="apartment_score">&bull; ' + apartment.score + '/5</span> \
+            <span class="apartment_reviews">&bull; '+ apartment.reviews_count + ' reviews</span> \
+          </a> \
+        </div> \
+      </div>'; 
     });
 
 
